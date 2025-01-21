f15df0e51a16d9e286f91796ff4dcd100080f64e
Gota Gando <gota@gando.dev>
2025-01-22 04:52:16 +0900

    Configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Zola HTML templates
+    "./content/**/*.md",     // Markdown files in Zola's content directory
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Lato', ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [
+    require("tailwindcss"),
+    require('@tailwindcss/typography'),
+    require("autoprefixer")
+  ],
+}
+
